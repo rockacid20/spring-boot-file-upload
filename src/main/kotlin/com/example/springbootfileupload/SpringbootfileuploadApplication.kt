be32eb14774898fcf7cc91a75fb5d69be3bd7aa1
rockacid20 <rockacid20@gmail.com>
2021-09-05 14:58:18 +0900

    init

---
+++ src/main/kotlin/com/example/springbootfileupload/SpringbootfileuploadApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootfileupload
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootfileuploadApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootfileuploadApplication>(*args)
+}
